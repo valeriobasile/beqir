f7a5aa0fd530f0bfc02db0b35387e875b9a7b900
roque lopez condori <rlopezco@farhady.unice.fr>
2017-03-22 11:54:37 +0100

    version 0.2

--- src/query_expander.py
+++ src/query_expander.py
@@ -3,15 +3,12 @@ import re
 from time import sleep
 from os.path import join, dirname
 from ngrams_api import runQuery
+from prettytable import PrettyTable
 
-def expand_query(words_path, patterns_path):
-    with open(join(dirname(__file__), words_path)) as fin:
-        word_list = [x.split("\t")[1].rstrip() for x in fin.readlines()]
-
-    with open(join(dirname(__file__), patterns_path)) as fin:
-        pattern_list = [x.rstrip() for x in fin.readlines()]
-
-    for word in word_list:
+def expand_query(synsets_path, patterns_path):
+    synset_list, pattern_list = read_data(synsets_path, patterns_path)
+    
+    for synset, word in synset_list:
         candidates = []
         for pattern in pattern_list:
             query = pattern.replace("[lemma]", word)
@@ -22,15 +19,54 @@ def expand_query(words_path, patterns_path):
                 candidates += runQuery(query)
                 sleep(10)
 
-        save_candidates(word, ", ".join(candidates))
+        save_candidates(synset, word, ", ".join(candidates))
 
-def save_candidates(word, candidates):
+def save_candidates(synset, word, candidates):
     candidates = re.sub("_[A-Z]+", "", candidates)
 
     with open(join(dirname(__file__), "../data/output/query_expanded.txt"), "a") as fout:
-        fout.write("%s: %s\n" % (word, candidates))
+        fout.write("%s\t%s\t%s\n" % (synset, word, candidates))
 
-if __name__ == '__main__':  
-    words_path = "../data/input/lemmas_imagenet.txt"
+def read_data(synsets_path, patterns_path):
+    synset_list = []
+    pattern_list = []
+
+    with open(join(dirname(__file__), synsets_path)) as fin:
+        for line in fin:
+            line_data = line.split("\t")
+            synset = line_data[0]
+            for word in line_data[1].split(","):
+                synset_list.append((synset, word.strip()))
+
+    with open(join(dirname(__file__), patterns_path)) as fin:
+        pattern_list = [x.rstrip() for x in fin.readlines()]
+
+    return synset_list, pattern_list
+
+def calculate_statistics():
+    synset_dict = {}
+    table = PrettyTable()
+    table.field_names = ["Synset", "Lemmas", "Expanded Lemmas"]
+    table.align = "r"
+
+    with open(join(dirname(__file__), "../data/output/query_expanded.txt")) as fin:
+        for line in fin:
+            line_data = line.rstrip().split("\t")
+            synset = line_data[0]
+            word = line_data[1]
+            expanded_words = line_data[2].split(", ") if len(line_data) > 2 else []
+            
+            if synset not in synset_dict: synset_dict[synset] = {'words':[], 'expanded_words':[]}
+            synset_dict[synset]['words'].append(word)
+            synset_dict[synset]['expanded_words'] += expanded_words
+
+    for synset in synset_dict:
+        table.add_row([synset, len(synset_dict[synset]['words']), len(set(synset_dict[synset]['expanded_words']))])
+    
+    print table.get_string(sortby="Lemmas")
+
+if __name__ == '__main__':
+    synsets_path = "../data/input/synset_imagenet_10.txt"
     patterns_path = "../data/input/patterns.txt"
-    expand_query(words_path, patterns_path)
+    #expand_query(synsets_path, patterns_path)
+    calculate_statistics()
