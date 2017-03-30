56d6ba1b32b51d641c1d81675b20056ea4482e62
roque lopez condori <rlopezco@farhady.unice.fr>
2017-03-30 14:35:47 +0200

    Adding functions to filter expansions

--- src/query_expander.py
+++ src/query_expander.py
@@ -4,6 +4,7 @@ from time import sleep
 from os.path import join, dirname
 from ngrams_api import runQuery
 from prettytable import PrettyTable
+from collections import Counter
 
 def expand_query(synsets_path, patterns_path):
     synset_list, pattern_list = read_data(synsets_path, patterns_path)
@@ -88,8 +89,53 @@ def calculate_synset_statistics():
     
     print table.get_string(sortby="Lemmas")
 
+def calculate_frequent_expansions(patterns_path, top=10):
+    expansions_frequency = {}
+    with open(join(dirname(__file__), patterns_path)) as fin:
+        expansions_frequency = {x.rstrip():[] for x in fin.readlines()}
+
+    with open(join(dirname(__file__), "../data/output/query_expanded_patterns.txt")) as fin:
+        for line in fin:
+            line_data = line.rstrip().split("\t")
+            word = line_data[1]
+            for pattern_expansions in line_data[2:]:
+                pattern_expansions = pattern_expansions.split(":")
+                pattern = pattern_expansions[0]
+                expansions = [x.strip() for x in pattern_expansions[1].replace(word, "").split(",")]
+                if len(expansions[0]) > 0:
+                    expansions_frequency[pattern] += expansions
+
+    for pattern in expansions_frequency.keys():
+        frequencies = Counter(expansions_frequency[pattern])
+        sorted_freq = sorted(frequencies.items(), key=lambda x:x[1], reverse=True)
+        expansions_frequency[pattern] = [x for x,y in sorted_freq[:top]]
+
+    return expansions_frequency
+
+def filter_expansions(patterns_path):
+    expansions_by_patterns = calculate_frequent_expansions(patterns_path)
+    patterns_to_filter = ["[lemma] in the *", "[lemma] is *_VERB", "[lemma] was *_VERB"]
+    frequent_expansions = []
+    line_list = []
+
+    for pattern in patterns_to_filter:
+        frequent_expansions += expansions_by_patterns[pattern]
+
+    with open(join(dirname(__file__), "../data/output/query_expanded.txt")) as fin:
+        for line in fin:
+            synset, word, expansions = line.split("\t")
+            expansions = expansions.rstrip()
+            new_frequent_expansions = {"%s %s" % (word, x) for x in frequent_expansions}
+            filtered_expansions = [x for x in expansions.split(", ") if x not in new_frequent_expansions]
+            line_list.append("%s\t%s\t%s" % (synset, word, ", ".join(filtered_expansions)))
+
+    with open(join(dirname(__file__), "../data/output/query_expanded_filtered.txt"), "w") as fout:
+        fout.write("\n".join(line_list))
+
+
 if __name__ == '__main__':
-    synsets_path = "../data/input/synset_imagenet_10.txt"
+    synsets_path = "../data/input/synset_imagenet.txt"
     patterns_path = "../data/input/patterns.txt"
-    expand_query(synsets_path, patterns_path)
-    calculate_synset_statistics()
+    #expand_query(synsets_path, patterns_path)
+    #calculate_synset_statistics()
+    filter_expansions(patterns_path)
