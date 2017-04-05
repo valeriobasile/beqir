033354977d5bf59a2c52b38d688a65f6ffe3487c
roque lopez condori <rlopezc27@gmail.com>
2017-04-05 11:58:06 +0200

    New outputs

--- src/query_expander.py
+++ src/query_expander.py
@@ -40,24 +40,13 @@ def save_expansions(synset, word, pattern_expansions):
     with open(join(dirname(__file__), "../data/output/raw_data/query_expansions_patterns.txt"), "a") as fout:
         fout.write("%s\t%s\t%s\n" % (synset, word, "\t".join(expasion_pattern_list)))
 
-def read_input_data(synsets_path, patterns_path):
-    synset_list = []
-    pattern_list = []
-
-    with open(join(dirname(__file__), synsets_path)) as fin:
-        for line in fin:
-            synset, words = line.rstrip().split("\t")
-            for word in words.split(", "):
-                synset_list.append((synset, word.strip()))
+def process_expansions(patterns_path, output_folder):
+    all_expansions = {}
+    order_expansions = []
+    patterns = []
 
     with open(join(dirname(__file__), patterns_path)) as fin:
-        pattern_list = [x.rstrip() for x in fin.readlines()]
-
-    return synset_list, pattern_list
-
-def process_data(patterns_path, expansions_path):
-    all_expansions = {}
-    order_data = []
+        patterns =[x.rstrip() for x in fin.readlines()]
 
     with open(join(dirname(__file__), "../data/output/raw_data/query_expansions_patterns.txt")) as fin:
         for line in fin:
@@ -65,7 +54,7 @@ def process_data(patterns_path, expansions_path):
             if synset not in all_expansions: all_expansions[synset] = {}
             if word not in all_expansions[synset]: 
                 all_expansions[synset][word] = {}
-                order_data.append((synset, word))
+                order_expansions.append((synset, word))
 
             for pattern_expansions in pattern_expansion_list:
                 pattern, expansions = pattern_expansions.split(":")
@@ -74,44 +63,29 @@ def process_data(patterns_path, expansions_path):
                 if len(expansions[0]) > 0:
                     all_expansions[synset][word][pattern].update(expansions)
 
-    create_outputs(all_expansions, order_data, patterns_path, expansions_path)
+    expansions_data = {'all_expansions':all_expansions, 'order_expansions':order_expansions, 'patterns':patterns}
+    create_outputs(expansions_data, output_folder)
+
+    return expansions_data
                     
-def create_outputs(all_expansions, order_data, patterns_path, expansions_path):
-    single_file = []
-    multiple_file = {}
+def filter_expansions(expansions_data, output_folder):
+    frequent_expansions = calculate_frequent_expansions(expansions_data)
+    patterns_to_filter = {"[lemma] in the *", "[lemma] is *_VERB", "[lemma] was *_VERB"}
 
-    with open(join(dirname(__file__), patterns_path)) as fin:
-        multiple_file = {x.rstrip():[] for x in fin.readlines()}
+    for synset, words_data in expansions_data['all_expansions'].items():
+        for word, patterns_data in expansions_data['all_expansions'][synset].items():
+            for pattern, expansions in expansions_data['all_expansions'][synset][word].items():
+                if pattern in patterns_to_filter:
+                    new_frequent_expansions = {"%s %s" % (word, x) for x in frequent_expansions[pattern]}
+                    filtered_expansions = {x for x in expansions if x not in new_frequent_expansions}
+                    expansions_data['all_expansions'][synset][word][pattern] = filtered_expansions
 
-    for synset, word in order_data:
-        expasion_pattern_list = []
-        for pattern, expansions in all_expansions[synset][word].items():
-            expasion_pattern_list.append("%s: %s" % (pattern, ", ".join(expansions)))
-            multiple_file[pattern].append("%s\t%s\t%s" % (synset, word, ", ".join(expansions)))
-        single_file.append("%s\t%s\t%s" % (synset, word, "\t".join(expasion_pattern_list)))
+    create_outputs(expansions_data, output_folder)
 
-    with open(join(dirname(__file__), expansions_path), "w") as fout:
-        fout.write("\n".join(single_file))
+def calculate_frequent_expansions(expansions_data, top=10):
+    expansions_frequency = {x.rstrip():[] for x in expansions_data['patterns']}
 
-    for pattern in multiple_file.keys():
-        with open(join(dirname(__file__), "../data/output/processed_data/%s.txt") % pattern, "w") as fout:
-            fout.write("\n".join(multiple_file[pattern]))
-
-def read_line(line):
-    line_data = line.rstrip().split("\t")
-    synset = line_data[0]
-    word = line_data[1]
-    pattern_expansion_list = line_data[2:]
-
-    return synset, word, pattern_expansion_list
-
-def calculate_frequent_expansions(patterns_path, expansions_path, top=10):
-    expansions_frequency = {}
-
-    with open(join(dirname(__file__), patterns_path)) as fin:
-        expansions_frequency = {x.rstrip():[] for x in fin.readlines()}
-
-    with open(join(dirname(__file__), expansions_path)) as fin:
+    with open(join(dirname(__file__), "../data/output/processed_data/all_expansions.txt")) as fin:
         for line in fin:
             synset, word, pattern_expansion_list = read_line(line)
             for pattern_expansions in pattern_expansion_list:
@@ -127,35 +101,6 @@ def calculate_frequent_expansions(patterns_path, expansions_path, top=10):
         expansions_frequency[pattern] = [x for x,y in sorted_freq[:top]]
 
     return expansions_frequency
-
-def filter_expansions(patterns_path, expansions_path):
-    expansions_by_patterns = calculate_frequent_expansions(patterns_path, expansions_path)
-    patterns_to_filter = ["[lemma] in the *", "[lemma] is *_VERB", "[lemma] was *_VERB"]
-    frequent_expansions = []
-    line_pattern_list = []
-
-    for pattern in patterns_to_filter:
-        frequent_expansions += expansions_by_patterns[pattern]
-
-    with open(join(dirname(__file__), expansions_path)) as fin:
-        for line in fin:
-            synset, word, pattern_expansion_list = read_line(line)
-            new_frequent_expansions = {"%s %s" % (word, x) for x in frequent_expansions}
-            pattern_filtered_expansions = []
-
-            for pattern_expansions in pattern_expansion_list:
-                pattern, expansions = pattern_expansions.split(":")
-                expansions = expansions.strip()
-                current_filtered_expansions = []
-
-                if len(expansions) > 0:
-                    current_filtered_expansions = [x for x in expansions.split(", ") if x not in new_frequent_expansions]
-                pattern_filtered_expansions.append("%s: %s" % (pattern, ", ".join(current_filtered_expansions)))
-            
-            line_pattern_list.append("%s\t%s\t%s" % (synset, word, "\t".join(pattern_filtered_expansions)))
-
-    with open(join(dirname(__file__), "../data/output/filtered_data/all_expansions_filtered.txt"), "w") as fout:
-        fout.write("\n".join(line_pattern_list))
 
 def calculate_statistics(expansions_path):
     synset_dict = {}
@@ -200,12 +145,59 @@ def print_synset_statistics(synset_dict):
     
     print table.get_string(sortby="Lemmas")
 
+def create_outputs(expansions_data, output_folder):
+    all_expansions = expansions_data['all_expansions']
+    order_expansions = expansions_data['order_expansions']
+    patterns = expansions_data['patterns']
+    single_file = []
+    multiple_file = {x:[] for x in patterns}
+
+    for synset, word in order_expansions:
+        expasion_pattern_list = []
+        for pattern, expansions in all_expansions[synset][word].items():
+            expasion_pattern_list.append("%s: %s" % (pattern, ", ".join(expansions)))
+            multiple_file[pattern].append("%s\t%s\t%s" % (synset, word, ", ".join(expansions)))
+        single_file.append("%s\t%s\t%s" % (synset, word, "\t".join(expasion_pattern_list)))
+
+    with open(join(dirname(__file__), "%s/all_expansions.txt") % output_folder, "w") as fout:
+        fout.write("\n".join(single_file))
+
+    for index, pattern in enumerate(patterns):
+        with open(join(dirname(__file__), "%s/pattern_%d.txt") % (output_folder, index+1), "w") as fout:
+            fout.write("\n".join(multiple_file[pattern]))
+
+    with open(join(dirname(__file__), "%s/pattern_names.txt") % output_folder, "w") as fout:
+        fout.write("\n".join(["pattern_%d: %s" % (i+1, x) for i,x in enumerate(patterns)]))      
+
+def read_input_data(synsets_path, patterns_path):
+    synset_list = []
+    pattern_list = []
+
+    with open(join(dirname(__file__), synsets_path)) as fin:
+        for line in fin:
+            synset, words = line.rstrip().split("\t")
+            for word in words.split(", "):
+                synset_list.append((synset, word.strip()))
+
+    with open(join(dirname(__file__), patterns_path)) as fin:
+        pattern_list = [x.rstrip() for x in fin.readlines()]
+
+    return synset_list, pattern_list
+
+def read_line(line):
+    line_data = line.rstrip().split("\t")
+    synset = line_data[0]
+    word = line_data[1]
+    pattern_expansion_list = line_data[2:]
+
+    return synset, word, pattern_expansion_list
 
 if __name__ == '__main__':
     synsets_path = "../data/input/synset_imagenet.txt"
     patterns_path = "../data/input/patterns.txt"
-    expansions_path = "../data/output/processed_data/all_expansions.txt"
-    expand_query(synsets_path, patterns_path)
-    process_data(patterns_path, expansions_path)
-    filter_expansions(patterns_path, expansions_path)
-    calculate_statistics("../data/output/filtered_data/all_expansions_filtered.txt")
+    processed_data_folder = "../data/output/processed_data/"
+    filtered_data_folder = "../data/output/filtered_data/"
+    #expand_query(synsets_path, patterns_path)
+    expansions_data = process_expansions(patterns_path, processed_data_folder)
+    filter_expansions(expansions_data, filtered_data_folder)
+    calculate_statistics("../data/output/filtered_data/all_expansions.txt")
